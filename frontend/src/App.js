ae5226b73c2adde1709b61d5eac7c6253c64af7f
yosef <yosef604@gmail.com>
2022-06-01 01:56:57 +0300

    header n footer design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import {Container} from 'react-bootstrap'
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -5,8 +6,10 @@ const App = () => {
   return (
   <>
     <Header />
-    <main>
+    <main className='py-3'>
+      <Container>
         <h1>Welcome to ProShop</h1>
+      </Container>
     </main>
     <Footer />
   </>
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import {Col, Container, Row} from 'react-bootstrap'
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <Container>
+        <Row>
+            <Col className='text-center py-3'>
+                Copyright &copy; ProShop
+            </Col>
+        </Row>
+    </Container>
   )
 }
 
