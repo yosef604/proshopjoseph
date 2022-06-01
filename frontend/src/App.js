dbd4306f6430433e6bc12df73f70fa90df195480
yosef <yosef604@gmail.com>
2022-06-02 02:43:48 +0300

    installing react-router-dom + r-r-bootstrap n implementing router in app.js + changing from a tag to link/link container tag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-  <>
+  <Router>
     <Header />
     <main className='py-3'>
       <Container>
-        <HomeScreen />
+        <Routes>
+           <Route path='/' element={<HomeScreen />} />
+           <Route path='/product/:id' element={<ProductScreen />} />
+        </Routes>
       </Container>
     </main>
     <Footer />
-  </>
+  </Router>
   );
 }
 
