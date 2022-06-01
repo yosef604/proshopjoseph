217e9a0796f0ce76dbc404a1638e359658ead734
yosef <yosef604@gmail.com>
2022-06-01 23:22:47 +0300

    adding stars to rating + styling + proptypes on a nutshell

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({product}) => {
   return (
@@ -15,13 +16,11 @@ const Product = ({product}) => {
         </a>
 
         <Card.Text as='div'>
-            <div className='my-3'>
-                {product.rating} from {product.numReviews} reviews
-            </div>
+            <Rating value={product.rating} text={`${product.numReviews} reviews`}/> 
         </Card.Text>
 
         <Card.Text as='h3'>
-            ${product.price}
+            {product.price}
         </Card.Text>
     </Card>
   )
