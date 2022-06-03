4ea853671fbd34faa5828f93188b3642669b7e30
yosef <yosef604@gmail.com>
2022-06-03 03:24:59 +0300

    fetcing the product/s from the backend through axios + warning faild prop types in the productscreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap'
 import { Link, useParams } from 'react-router-dom'
 import Rating from '../components/Rating'
-import products from '../products'
+
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({})
   const {id} = useParams()
-  const product = products.find(p => p._id === id)
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const {data} = await axios.get(`/api/products/${id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [])
+
   return (
     <>
       <Link className='btn btn-dark my-3' to='/'>
