bba0ce0bc86d168051432b7db4de8d8714080966
yosef <yosef604@gmail.com>
2022-06-06 23:57:02 +0300

    adding redux store

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,8 +10,6 @@ const router = express.Router()
 // @access  Public
 router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({})
-    
-
     res.json(products)
 }))
 
@@ -20,8 +18,6 @@ router.get('/', asyncHandler(async (req, res) => {
 // @access  Public
 router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
-
-    console.log(req.params.id)
 
     if (product) {
         res.json(product)
