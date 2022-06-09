88afd9c271b472e76142ba4f1f2406fa9ca4d143
yosef <yosef604@gmail.com>
2022-06-09 04:31:18 +0300

    authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,3 +26,23 @@ export const authUser = asyncHandler(async (req, res) => {
 
 
 })
+
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Privet
+export const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
