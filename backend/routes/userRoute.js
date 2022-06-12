1ebf751b77f04e1172a987101f5357311067338f
yosef <yosef604@gmail.com>
2022-06-13 01:44:06 +0300

    user update profile - backpoint

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { authUser, getUserProfile, registerNewUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerNewUser, updateUserProfile } from '../controllers/userController.js'
 import protect from '../middleware/authNiddleware.js'
 
 const router = express.Router()
 
 router.route('/').post(registerNewUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 export default router
