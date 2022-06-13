4ded6c48e3c71ce925daaed2d74d927b9ac3354e
yosef <yosef604@gmail.com>
2022-06-13 15:45:00 +0300

    payment method - con, action, redcer & screen

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import { Button, Col, Form } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { saveShippingAddressAction, saveSPaymentMethodAction } from '../actions/cartActions'
+import CheckoutSteps from '../components/CheckoutSteps'
+import FormContainer from '../components/FormContainer'
+
+const PaymentScreen = () => {
+
+    const navigate = useNavigate()
+    
+    const cart = useSelector(state => state.cart)
+    const {shippingAddress} = cart
+
+    if (!shippingAddress) {
+        navigate('/shipping')
+    }
+
+    const [paymentMethod, setPaymentMethod] = useState('paypal')
+
+    const dispatch = useDispatch()
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(saveSPaymentMethodAction(paymentMethod))
+        navigate('/placeorder')
+    }
+  return (
+    <FormContainer>
+        <CheckoutSteps step1 step2 step3 />
+         <h1>Payment Method</h1>
+        <Form onSubmit={submitHandler}>
+            <Form.Group>
+                <Form.Label as='legend'>
+                    Select Method
+                </Form.Label>
+            
+                <Col>
+                    <Form.Check type='radio' label='PayPal or Credit Card' 
+                    name='paymentMethod' value='PayPal' checked onChange={
+                        (e) => setPaymentMethod(e.target.value)} id='PayPal'>
+
+                    </Form.Check>
+
+                    <Form.Check type='radio' label='Stripe' 
+                    name='paymentMethod' value='Stripe' onChange={
+                        (e) => setPaymentMethod(e.target.value)} id='Stripe'>
+
+                    </Form.Check>
+                </Col>
+            </Form.Group>
+           
+            <Button type='submit' variant='primary'>
+                Continue
+            </Button>
+        </Form>
+    </FormContainer>
+  )
+}
+
+export default PaymentScreen
