a1593555b5ec730bbd9408c0b40186a75f695c79
yosef <yosef604@gmail.com>
2022-06-15 01:43:16 +0300

    update order to paid - backpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { addOrderItemss, getOrderById } from '../controllers/orderController.js'
+import { addOrderItemss, getOrderById, updateOrderToPaid } from '../controllers/orderController.js'
 import protect from '../middleware/authNiddleware.js'
 
 const router = express.Router()
 
 router.route('/').post(protect, addOrderItemss)
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id.pay').put(protect, updateOrderToPaid)
 
 
 export default router
