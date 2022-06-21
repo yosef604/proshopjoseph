8b46149216411d3e8aefb22a7cce6b7bce050553
yosef <yosef604@gmail.com>
2022-06-21 04:12:08 +0300

    preparing the server for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
-
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
 app.use('/api/orders', orderRouter)
@@ -35,10 +31,25 @@ app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
+
 const __dirname = path.resolve()
 app.use('/uploadsFolder', express.static(
     path.join(__dirname, '/uploadsFolder')
 ))
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(
+        path.resolve(__dirname, 'frontend', 'build', 'index.html')
+    ))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running')
+    })
+}
+
 
 app.use(notFound)
 app.use(errorHandler)
